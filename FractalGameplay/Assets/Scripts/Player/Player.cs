d34e5dd8aaca1d916ccfadf76b43f027a19e4535
Amyfiz <solonukhina@sfedu.ru>
2023-11-20 21:45:33 +0300

    feat: Add Player and PlayerController scripts

---
+++ FractalGameplay/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //variables for moving player left and right
+    [SerializeField] public float playerSpeed;
+    [SerializeField] public float moveInput;
+
+    //variable for flipping player texture
+    [SerializeField] public bool facingRight = true;
+    [SerializeField] public bool isGrounded = true;
+    [SerializeField] public Transform feetPosition;
+    [SerializeField] public float checkRadius;
+    [SerializeField] public LayerMask whatIsGrounded;
+}
