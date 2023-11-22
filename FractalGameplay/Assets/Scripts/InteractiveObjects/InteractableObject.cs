057d80b32d65c3b608a98f7b3c511eb785a6f946
KreKer_ <platon.bob@gmail.com>
2023-11-22 22:26:53 +0300

    feat: Add InteractableObject script
    Player can now interact with objects on scene

--- FractalGameplay/Assets/Scripts/InteractiveObjects/InteractableObject.cs
+++ FractalGameplay/Assets/Scripts/InteractiveObjects/InteractableObject.cs
@@ -1,16 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InteractableObject : MonoBehaviour, IInteractable
 {
-
+    private Collider2D clldr;
+    private Player player;  
     
+
+
+    void Awake()
+    {
+        clldr = gameObject.GetComponent<Collider2D>();
+        player = GameObject.FindObjectOfType<Player>();
+    }
+
+    public void Update()
+    {
+        Interact();
+    }
+
     public void Interact()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (clldr.IsTouching(player.GetComponent<Collider2D>()) && Input.GetKeyDown(KeyCode.F))
         {
-
+            //Instantiate(gameObject.GetComponent<Text>());
         }
     }
+
+    
 }
